a56014d83223dab84b0fc6eee38b06e95084f2e6
harryzhang1018 <hzhang699@wisc.edu>
2022-10-09 15:35:04 -0500

    assignment03 final push

--- assignment03/windmill_gl.js
+++ assignment03/windmill_gl.js
@@ -2,24 +2,66 @@ function setup() {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
     var slider1 = document.getElementById('slider1');
-    slider1.value = 0;
-
+    slider1.value = 20;
+    var angle = 0;
+    var cloud_pos1 = 20;
+    var cloud_pos2 = 220;
+    var cloud_pos3 = 420;
     function draw() {
+    requestAnimationFrame(draw);
 	canvas.width = canvas.width;
-	// use the sliders to get the angles
-	var tParam = slider1.value*0.01;
+	// use the sliders values, which would further decided
+    // the movement speed of clouds and blades
+	var wind_intensity = slider1.value*0.05;
+    angle += wind_intensity*0.01;
+    cloud_pos1 +=  wind_intensity * 0.1;
+    cloud_pos2 += wind_intensity * 0.125;
+    cloud_pos3 += wind_intensity * 0.1;
+    // these if conditions are used to judge if clods are out of boundries
+    if (cloud_pos1>500){
+        cloud_pos1 = 0;
+    }
+    if (cloud_pos2>500){
+        cloud_pos2 = 0;
+    }
+    if (cloud_pos3>500){
+        cloud_pos3 = 0;
+    }
 	
+    // basic functions of using matrix transformation
 	function moveToTx(loc,Tx)
 	{var res=vec2.create(); vec2.transformMat3(res,loc,Tx); context.moveTo(res[0],res[1]);}
 
 	function lineToTx(loc,Tx)
 	{var res=vec2.create(); vec2.transformMat3(res,loc,Tx); context.lineTo(res[0],res[1]);}
 	
+    //draw a house body
+    function house(Tx){
+        context.beginPath();
+        context.strokeStyle = "black";
+        context.fillStyle = "#800";
+        moveToTx([0,0],Tx);
+        lineToTx([-40,70],Tx);
+        lineToTx([40,70],Tx);
+        context.closePath();context.fill();context.stroke();
+        moveToTx([0,0],Tx);lineToTx([40,70],Tx);lineToTx([70,45],Tx);lineToTx([30,-25],Tx);
+        context.closePath();context.fill();context.stroke();
+    }
+    // draw another part of house
+    function house_2(Tx){
+        context.beginPath();
+        context.fillStyle = "gray";
+        moveToTx([-40,70],Tx);
+        context.rect(160,220,80,80);context.fill();context.stroke();
+        moveToTx([40,70],Tx);lineToTx([70,45],Tx);lineToTx([70,125],Tx);
+        lineToTx([40,150],Tx);context.closePath();context.fill();context.stroke();
 
+    }
+    // draw a single blade
     function drawblade(Tx){
         context.beginPath();
-        context.fillStyle = orange;
-        context.strokeStyle = orange;
+        context.fillStyle = "orange";
+        context.strokeStyle = "orange";
         moveToTx([0,0],Tx);
         lineToTx([0,20],Tx);
         context.stroke();
@@ -30,95 +72,52 @@ function setup() {
         context.closePath();
         context.fill();
     }
-
-    function drawfourblades()
-
-	function drawObject(color,Tx) {
-	    context.beginPath();
-	    context.fillStyle = color;
-	    moveToTx([-5,-5],Tx);
-	    lineToTx([-5,5],Tx);
-            lineToTx([5,5],Tx);
-      	    lineToTx([10,0],Tx);
-	    lineToTx([5,-5],Tx);
-	    context.closePath();
-	    context.fill();
-	}
-	
-	function drawAxes(color,Tx) {
-	    context.strokeStyle=color;
-	    context.beginPath();
-	    // Axes
-	    moveToTx([120,0],Tx);lineToTx([0,0],Tx);lineToTx([0,120],Tx);
-	    // Arrowheads
-	    moveToTx([110,5],Tx);lineToTx([120,0],Tx);lineToTx([110,-5],Tx);
-	    moveToTx([5,110],Tx);lineToTx([0,120],Tx);lineToTx([-5,110],Tx);
-	    // X-label
-	    moveToTx([130,0],Tx);lineToTx([140,10],Tx);
-	    moveToTx([130,10],Tx);lineToTx([140,0],Tx);
-	    context.stroke();
-	}
-
-	var Rstart = 50.0;
-	var Rslope = 25.0;
-	var Cspiral = function(t) {
-	    var R = Rslope * t + Rstart;
-	    var x = R * Math.cos(2.0 * Math.PI * t);
-	    var y = R * Math.sin(2.0 * Math.PI * t);
-	    return [x,y];
-	}
-	var Cspiral_tangent = function(t) {
-	    var R = Rslope * t + Rstart;
-	    var Rprime = Rslope;
-	    var x = Rprime * Math.cos(2.0 * Math.PI * t)
-                - R * 2.0 * Math.PI * Math.sin(2.0 * Math.PI * t);
-	    var y = Rprime * Math.sin(2.0 * Math.PI * t)
-                + R * 2.0 * Math.PI * Math.cos(2.0 * Math.PI * t);
-	    return [x,y];
-	}
-
-	function drawTrajectory(t_begin,t_end,intervals,C,Tx,color) {
-	    context.strokeStyle=color;
-	    context.beginPath();
-            moveToTx(C(t_begin),Tx);
-            for(var i=1;i<=intervals;i++){
-		var t=((intervals-i)/intervals)*t_begin+(i/intervals)*t_end;
-		lineToTx(C(t),Tx);
-            }
-            context.stroke();
-	}
-
-	// make sure you understand these    
-
-	drawAxes("black", mat3.create());
-	
-	var Tred_to_canvas = mat3.create();
-	mat3.fromTranslation(Tred_to_canvas,[25,25]);
-	drawAxes("red",Tred_to_canvas);
-	drawObject("red",Tred_to_canvas);
-    drawblade("orange",Tred_to_canvas);
-    //var Tred_rotate = mat3.create();
-    mat3.rotate(Tred_to_canvas,Tred_to_canvas,-Math.PI/2);
-    drawblade("blue",Tred_to_canvas);
-
-	var Tblue_to_canvas = mat3.create();
-	mat3.fromTranslation(Tblue_to_canvas,[150,175]);
-	mat3.scale(Tblue_to_canvas,Tblue_to_canvas,[1,-1]); // Flip the Y-axis
-	drawAxes("blue",Tblue_to_canvas);
-	
-	drawTrajectory(0.0,2.0,100,Cspiral,Tblue_to_canvas,"brown");
-	var Tgreen_to_blue = mat3.create();
-	mat3.fromTranslation(Tgreen_to_blue,Cspiral(tParam));
-	var tangent = Cspiral_tangent(tParam);
-	var angle = Math.atan2(tangent[1],tangent[0]);
-	mat3.rotate(Tgreen_to_blue,Tgreen_to_blue,angle);
-	var Tgreen_to_canvas = mat3.create();
-	mat3.multiply(Tgreen_to_canvas, Tblue_to_canvas, Tgreen_to_blue);
-	// drawAxes("green",Tgreen_to_canvas); // Un-comment this to view axes
-	drawObject("green",Tgreen_to_canvas);
+    //draw four blades by calling drawblade() and apply matrix rotate here
+    function drawfourblades(T_blade_to_canvas){
+        
+        drawblade(T_blade_to_canvas);
+        mat3.rotate(T_blade_to_canvas,T_blade_to_canvas,-Math.PI/2);
+        drawblade(T_blade_to_canvas);
+        mat3.rotate(T_blade_to_canvas,T_blade_to_canvas,-Math.PI/2);
+        drawblade(T_blade_to_canvas);
+        mat3.rotate(T_blade_to_canvas,T_blade_to_canvas,-Math.PI/2);
+        drawblade(T_blade_to_canvas);
     }
 
-    slider1.addEventListener("input",draw);
+    // spining the blades by call drawfourblades() and apply a rotating angle
+    function spin_blades(angle,Tx){
+        mat3.rotate(Tx,Tx,angle);
+        drawfourblades(Tx);
+    }
+    // draw backgound sky blue color
+    function draw_background_upper(){
+        context.fillStyle = 'rgb(0,255,255)';
+        context.rect(0,0,500,300);
+        context.fill();    
+    }
+    // import clode image and draw them multple times
+    function draw_cloud(cloud_pos1,cloud_pos2,cloud_pos3){
+        cloud_img = new Image();
+        cloud_img.src = 'cloud.png';
+        context.drawImage(cloud_img,cloud_pos1,50,80,60);
+        context.drawImage(cloud_img,cloud_pos2,25,80,60);
+        context.drawImage(cloud_img,cloud_pos3,75,80,60);
+    }
+
+    draw_background_upper();
+    draw_cloud(cloud_pos1,cloud_pos2,cloud_pos3);
+    var T_house_to_canvas = mat3.create();
+    mat3.fromTranslation(T_house_to_canvas,[200,150]);
+    house(T_house_to_canvas);
+    house_2(T_house_to_canvas);
+
+    var T_blade_to_canvas = mat3.create();
+    mat3.fromTranslation(T_blade_to_canvas,[200,150]);
+    spin_blades(angle,T_blade_to_canvas);
+
+    }
+
+    //slider1.addEventListener("input",draw);
     draw();
 }
 window.onload = setup;
