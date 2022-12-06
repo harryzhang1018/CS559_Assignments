99afd330aac8eb3eba6b851b8194f265e14acef7
harry <hzhang699@wisc.edu>
2022-12-06 09:22:44 -0600

    hw7 push

--- assignment05/Drone.js
+++ assignment05/Drone.js
@@ -7,17 +7,23 @@ function setup() {
     slider1.value = 0;
     var slider2 = document.getElementById('slider2');
     slider2.value = 0;
-
+    var slider3 = document.getElementById('slider3');
+    slider3.value = 0;
+    var rot_angle = 0;
     var context = cameraContext; // default to drawing in the camera window
-
+    var tParam = 0;
     function draw() {
-      
+    requestAnimationFrame(draw);
     // clear both canvas instances
 	observerCanvas.width = observerCanvas.width;
 	cameraCanvas.width = cameraCanvas.width;
-
+    rot_angle += 0.5;
 	// use the sliders to get the angles
-	var tParam = slider1.value*0.01;
+	tParam += slider1.value*0.0000005;
+    var shape_twist = slider3.value;
+    if (tParam > 3){
+		tParam = 0.0;
+	}
     var viewAngle = slider2.value*0.02*Math.PI;
      
 	function moveToTx(loc,Tx)
@@ -31,14 +37,91 @@ function setup() {
         mat4.scale(Tx,Tx,[scale,scale,scale]);
         context.beginPath();
 	    context.fillStyle = color;
-	    moveToTx([-.05,-.05,0],Tx);
-	    lineToTx([-.05,.05,0],Tx);
-        lineToTx([.05,.05,0],Tx);
-      	lineToTx([.1,0,0],Tx);
-	    lineToTx([.05,-.05,0],Tx);
+	    moveToTx([-.05,0,-.05],Tx);
+	    lineToTx([-.05,0,.05],Tx);
+        lineToTx([.05,0,.05],Tx);
+      	//lineToTx([.1,0,0],Tx);
+	    lineToTx([.05,0,-.05],Tx);
 	    context.closePath();
 	    context.fill();
+        context.beginPath();
+        context.lineWidth = 2;
+        moveToTx([-.04,0,-.04],Tx);
+        lineToTx([-.1,0,-.1],Tx);
+        moveToTx([-.04,0,.04],Tx);
+        lineToTx([-.1,0,.1],Tx);
+        moveToTx([.04,0,-.04],Tx);
+        lineToTx([.1,0,-.1],Tx);
+        moveToTx([.04,0,.04],Tx);
+        lineToTx([.1,0,.1],Tx);
+        //adding a "holder" for blades at four corners
+        moveToTx([-.1,0,-.1],Tx);
+        lineToTx([-.1,0.05,-.1],Tx);
+        moveToTx([-.1,0,.1],Tx);
+        lineToTx([-.1,0.05,.1],Tx);
+        moveToTx([.1,0,-.1],Tx);
+        lineToTx([.1,0.05,-.1],Tx);
+        moveToTx([.1,0,.1],Tx);
+        lineToTx([.1,0.05,.1],Tx);
+        context.stroke();
+
+        drawBlades(Tx,rot_angle);
+        
 	}
+
+    function drawBlades(Tx,rot_angle){
+        context.beginPath();
+        context.strokeStyle="black";
+        var blade_end1 = vec3.create();
+        blade_end1 = [-0.1,0.05,-0.15];
+        vec3.rotateY(blade_end1,blade_end1,[-0.1,0.05,-0.1],rot_angle);
+        var blade_end2 = vec3.create();
+        blade_end2 = [-0.1,0.05,-0.05];
+        vec3.rotateY(blade_end2,blade_end2,[-0.1,0.05,-0.1],rot_angle);
+        moveToTx([-0.1,0.05,-0.1],Tx);
+        lineToTx(blade_end1,Tx);
+        lineToTx(blade_end2,Tx);
+        context.stroke();
+
+        context.beginPath();
+        context.strokeStyle="black";
+        var blade_end3 = vec3.create();
+        blade_end3 = [0.1,0.05,-0.15];
+        vec3.rotateY(blade_end3,blade_end3,[0.1,0.05,-0.1],rot_angle+0.1);
+        var blade_end4 = vec3.create();
+        blade_end4 = [0.1,0.05,-0.05];
+        vec3.rotateY(blade_end4,blade_end4,[0.1,0.05,-0.1],rot_angle+0.1);
+        moveToTx([0.1,0.05,-0.1],Tx);
+        lineToTx(blade_end3,Tx);
+        lineToTx(blade_end4,Tx);
+        context.stroke();
+
+        context.beginPath();
+        context.strokeStyle="black";
+        var blade_end5 = vec3.create();
+        blade_end5 = [0.1,0.05,0.15];
+        vec3.rotateY(blade_end5,blade_end5,[0.1,0.05,0.1],rot_angle+0.1);
+        var blade_end6 = vec3.create();
+        blade_end6 = [0.1,0.05,0.05];
+        vec3.rotateY(blade_end6,blade_end6,[0.1,0.05,0.1],rot_angle+0.1);
+        moveToTx([0.1,0.05,0.1],Tx);
+        lineToTx(blade_end5,Tx);
+        lineToTx(blade_end6,Tx);
+        context.stroke();
+
+        context.beginPath();
+        context.strokeStyle="black";
+        var blade_end7 = vec3.create();
+        blade_end7 = [-0.1,0.05,0.15];
+        vec3.rotateY(blade_end7,blade_end7,[-0.1,0.05,0.1],rot_angle+0.1);
+        var blade_end8 = vec3.create();
+        blade_end8 = [-0.1,0.05,0.05];
+        vec3.rotateY(blade_end8,blade_end8,[-0.1,0.05,0.1],rot_angle+0.1);
+        moveToTx([-0.1,0.05,0.1],Tx);
+        lineToTx(blade_end7,Tx);
+        lineToTx(blade_end8,Tx);
+        context.stroke();
+    }
 	
     function drawCamera(color,TxU,scale) {
         var Tx = mat4.clone(TxU);
@@ -85,31 +168,6 @@ function setup() {
 	    context.stroke();
 	}
 
-    function drawUVWAxes(color,TxU,scale) {
-        var Tx = mat4.clone(TxU);
-        mat4.scale(Tx,Tx,[scale,scale,scale]);
-
-        context.strokeStyle=color;
-	    context.beginPath();
-	    // Axes
-	    moveToTx([1.2,0,0],Tx);lineToTx([0,0,0],Tx);lineToTx([0,1.2,0],Tx);
-        moveToTx([0,0,0],Tx);lineToTx([0,0,1.2],Tx);
-	    // Arrowheads
-	    moveToTx([1.1,.05,0],Tx);lineToTx([1.2,0,0],Tx);lineToTx([1.1,-.05,0],Tx);
-	    moveToTx([.05,1.1,0],Tx);lineToTx([0,1.2,0],Tx);lineToTx([-.05,1.1,0],Tx);
-      	moveToTx([.05,0,1.1],Tx);lineToTx([0,0,1.2],Tx);lineToTx([-.05,0,1.1],Tx);
-	    // U-label
-        moveToTx([1.3,.05,0],Tx);lineToTx([1.3,-.035,0],Tx);lineToTx([1.35,-.05,0],Tx);
-        lineToTx([1.4,-.035,0],Tx);lineToTx([1.4,.05,0],Tx);
-        // V-label
-        moveToTx([-.05,1.4,0],Tx);lineToTx([0,1.3,0],Tx);lineToTx([.05,1.4,0],Tx);
-	    // W-label
-	    moveToTx([-.1,0,1.3],Tx);lineToTx([-.05,0,1.4],Tx);lineToTx([-0,0,1.3],Tx);
-	    lineToTx([.05,0,1.4],Tx);lineToTx([.1,0,1.3],Tx);
-
-	    context.stroke();
-	}
-
     function draw2DAxes(color,Tx) {
 	    context.strokeStyle=color;
 	    context.beginPath();
@@ -127,14 +185,6 @@ function setup() {
 	    context.stroke();
 	}
 
-    function drawUpVector(color,vecUp,Tx) {
-	    context.strokeStyle=color;
-	    context.beginPath();
-	    // A single line segment in the "up" direction
-	    moveToTx([0,0,0],Tx);
-        lineToTx(vecUp,Tx);
-	    context.stroke();
-    }
     var Hermite = function(t) {
 	    return [
 		2*t*t*t-3*t*t+1,
@@ -143,15 +193,6 @@ function setup() {
 		t*t*t-t*t
 	    ];
 	}
-
-    var HermiteDerivative = function(t) {
-        return [
-        6*t*t-6*t,
-        3*t*t-4*t+1,
-        -6*t*t+6*t,
-        3*t*t-2*t
-        ];
-    }
 
 	function Cubic(basis,P,t){
 	    var b = basis(t);
@@ -165,7 +206,7 @@ function setup() {
 	
 
 
-	var p0=[0,0,0];
+	var p0=[20-shape_twist*4.5,-20+shape_twist*4,shape_twist*4];
 	var d0=[100,300,0];
 	var p1=[100,100,-50];
 	var d1=[-100,300,0];
@@ -179,9 +220,6 @@ function setup() {
 	var C0 = function(t_) {return Cubic(Hermite,P0,t_);};
 	var C1 = function(t_) {return Cubic(Hermite,P1,t_);};
 	var C2 = function(t_) {return Cubic(Hermite,P2,t_);};
-	var C0prime = function(t_) {return Cubic(HermiteDerivative,P0,t_);};
-	var C1prime = function(t_) {return Cubic(HermiteDerivative,P1,t_);};
-    var C2prime = function(t_) {return Cubic(HermiteDerivative,P2,t_);};
       
     // three piece-wise parametric combine together (position)
 	var Ccomp = function(t) {
@@ -198,23 +236,6 @@ function setup() {
         return C2(u);
             }        
     }
-
-    // three piece-wise parametric combine together (oritation)
-	var Ccomp_tangent = function(t) {
-		if (t<1){
-			var u = t;
-			return C0prime(u);
-			} 
-		else if (t>1 && t<2) {
-			var u = t-1.0;
-			return C1prime(u);
-			}  
-		else if (t>2 && t<3){
-			var u = t - 2.0;
-			return C2prime(u);
-			}        
-	}
-
 
     var CameraCurve = function(angle) {
         var distance = 120.0;
@@ -236,11 +257,18 @@ function setup() {
         context.stroke();
 	}
 
+
+    // Create model(ing) transform
+    // (from moving object to world)
+    var Tmodel = mat4.create();
+	mat4.fromTranslation(Tmodel,Ccomp(tParam));
+
     // create two lookAt transforms; one for the camera
     // and one for the "external observer"
 
     // Create Camera (lookAt) transform
-     var eyeCamera = CameraCurve(viewAngle);
+    //var eyeCamera = Ccomp(tParam);
+    var eyeCamera = CameraCurve(viewAngle);
     var targetCamera = vec3.fromValues(0,0,0); // Aim at the origin of the world coords
     var upCamera = vec3.fromValues(0,100,0); // Y-axis of world coords to be vertical
 	var TlookAtCamera = mat4.create();
@@ -269,8 +297,6 @@ function setup() {
     // (orthographic for now)
     var TprojectionCamera = mat4.create();
     mat4.ortho(TprojectionCamera,-100,100,-100,100,-1,1);
-    //mat4.perspective(TprojectionCamera,Math.PI/4,1,-1,1); // Use for perspective teaser!
-
     // Create Observer projection transform
     // (orthographic for now)
     var TprojectionObserver = mat4.create();
@@ -285,13 +311,7 @@ function setup() {
     mat4.multiply(tVP_PROJ_VIEW_Observer,Tviewport,TprojectionObserver);
     mat4.multiply(tVP_PROJ_VIEW_Observer,tVP_PROJ_VIEW_Observer,TlookAtObserver);
       
-	// Create model(ing) transform
-    // (from moving object to world)
-    var Tmodel = mat4.create();
-	mat4.fromTranslation(Tmodel,Ccomp(tParam));
-    var tangent = Ccomp_tangent(tParam);
-    var angle = Math.atan2(tangent[1],tangent[0]);
-	mat4.rotateZ(Tmodel,Tmodel,angle);
+	
 
     // Create transform t_VP_PROJ_VIEW_MOD that incorporates
     // Viewport, projection, camera, and modeling transform
@@ -310,22 +330,23 @@ function setup() {
     draw2DAxes("black", mat4.create());
 	draw3DAxes("grey",tVP_PROJ_VIEW_Camera,100.0);
     // drawUpVector("orange",upCamera,tVP_PROJ_VIEW_Camera,1.0);
-	drawTrajectory(0.0,1.0,100,C0,tVP_PROJ_VIEW_Camera,"red");
+	drawTrajectory(0.0,1.0,100,C0,tVP_PROJ_VIEW_Camera,"blue");
     drawTrajectory(0.0,1.0,100,C1,tVP_PROJ_VIEW_Camera,"blue");
-    drawTrajectory(0.0,1.0,100,C2,tVP_PROJ_VIEW_Camera,"black");
+    drawTrajectory(0.0,1.0,100,C2,tVP_PROJ_VIEW_Camera,"blue");
     // draw3DAxes("green", tVP_PROJ_VIEW_MOD_Camera,100.0); // Uncomment to see "model" coords
     drawObject("green",tVP_PROJ_VIEW_MOD_Camera,100.0);
+
       
     // Draw the following in the Observer window
     context = observerContext;
 	draw3DAxes("grey",tVP_PROJ_VIEW_Observer,100.0);  
     // drawUpVector("orange",upCamera,tVP_PROJ_VIEW_Observer,1.0);
-    drawTrajectory(0.0,1.0,100,C0,tVP_PROJ_VIEW_Observer,"red");
+    drawTrajectory(0.0,1.0,100,C0,tVP_PROJ_VIEW_Observer,"blue");
     drawTrajectory(0.0,1.0,100,C1,tVP_PROJ_VIEW_Observer,"blue");
-    drawTrajectory(0.0,1.0,100,C2,tVP_PROJ_VIEW_Observer,"black");
+    drawTrajectory(0.0,1.0,100,C2,tVP_PROJ_VIEW_Observer,"blue");
     drawObject("green",tVP_PROJ_VIEW_MOD1_Observer,100.0);     
-    drawCamera("purple",tVP_PROJ_VIEW_MOD2_Observer,10.0); 
-	drawUVWAxes("purple",tVP_PROJ_VIEW_MOD2_Observer,100.0);  
+    drawCamera("purple",tVP_PROJ_VIEW_MOD2_Observer,8.0); 
+	//drawUVWAxes("purple",tVP_PROJ_VIEW_MOD2_Observer,100.0);  
     }
     
   
